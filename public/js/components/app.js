b33766b04d8a278bd48ff47e365c4a793a8116a3
pglewis <pglewis13@gmail.com>
2024-12-28 19:42:44 -0500

    Include the search string in the results info

--- public/js/components/app.js
+++ public/js/components/app.js
@@ -59,14 +59,17 @@ function Info(props) {
 	const {store} = props;
 	const {results} = store.getState();
 	const {lastUpdated} = store.getState().rankings;
-	const {topN, recentInDays} = store.getState().filters;
+	const {topN, recentInDays, search} = store.getState().filters;
 
 	const rootElement = getTemplateElement("#info-template");
 	const info = rootElement.querySelector(".result-info");
 	const refreshed = rootElement.querySelector(".refreshed");
 
-	info.textContent = `Showing ${results.length} results in the top ${topN} set in the past ${recentInDays} day(s)`;
 	refreshed.textContent = `Last refreshed: ${lastUpdated} (UTC)`;
+	info.textContent =
+		`Showing ${results.length} ${results.length === 1 ? "result" : "results"} `
+		+ `${search ? ' matching "' + search + '"' : ""} `
+		+ `in the top ${topN} set in the past ${recentInDays} day(s) `;
 
 	return rootElement;
 }
